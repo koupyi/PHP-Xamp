75aa2e0bdc02bec54538c5793ae4008905c38847
koupyi <koupyi614@gmail.com>
2026-03-02 14:58:36 -0300

    32gigas

---
+++ views/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="?p=home"><i class="bi bi-house-door-fill"></i></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="?p=categorias">
+                    <i class="bi bi-list-task"></i> Consultar Categorias
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?p=cliente">
+                    <i class="bi bi-list-task"></i> Consultar Clientes
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?p=fornecedor">
+                    <i class="bi bi-list-task"></i> Consultar Fornecedor
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
